846122beb2a04401891d999e327ac04f4385959a
Diego <diegopalvarez@gmail.com>
2026-07-09 14:07:52 +0200

    fix: add default request type to HTTPClient factory

--- shared/src/commonMain/kotlin/com/diegopalvarez/oreplay/core/network/createHttpClient.kt
+++ shared/src/commonMain/kotlin/com/diegopalvarez/oreplay/core/network/createHttpClient.kt
@@ -3,8 +3,11 @@ package com.diegopalvarez.oreplay.core.network
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -19,6 +22,11 @@ fun createHttpClient(engine: HttpClientEngine): HttpClient {
                     ignoreUnknownKeys = true
                 }
             )
+        }
+
+        // Set up default request type to JSON
+        defaultRequest {
+            contentType(ContentType.Application.Json)
         }
     }
 }
