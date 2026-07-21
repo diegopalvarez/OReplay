e51567d020a9b0d89943c251f4167831cf0596f7
Diego <diegopalvarez@gmail.com>
2026-07-21 13:20:29 +0200

    feat: create Events mapper and fix data types in Event domain model

---
+++ shared/src/commonMain/kotlin/com/diegopalvarez/oreplay/data/mappers/remote/RemoteEventMapper.kt
@@ -0,0 +1,40 @@
+package com.diegopalvarez.oreplay.data.mappers.remote
+
+import com.diegopalvarez.oreplay.data.mappers.util.getInstant
+import com.diegopalvarez.oreplay.data.mappers.util.getLocalDate
+import com.diegopalvarez.oreplay.data.mappers.util.getTimezone
+import com.diegopalvarez.oreplay.data.remote.dto.events.RemoteEvent
+import com.diegopalvarez.oreplay.data.remote.dto.events.RemoteEventResponse
+import com.diegopalvarez.oreplay.domain.model.Event
+
+private fun getEvent(remoteEvent: RemoteEvent): Event{
+    return Event(
+        id = remoteEvent.id,
+        isHidden = remoteEvent.isHidden,
+        picture = remoteEvent.picture,
+        website = remoteEvent.website,
+        scope = remoteEvent.scope,
+        location = remoteEvent.location,
+        countryCode = remoteEvent.countryCode,
+        initialDate = getLocalDate(remoteEvent.initialDate),
+        finalDate = getLocalDate(remoteEvent.finalDate),
+        timezone = getTimezone(remoteEvent.timezone),
+        federation = remoteEvent.federationID,
+        created = getInstant(remoteEvent.created),
+        modified = getInstant(remoteEvent.modified),
+        organizer = getOrganizer(remoteEvent.organizer),
+        selfLink = remoteEvent.links.self.href
+        // The stages are missing and must be added when parsed
+    )
+}
+
+/**
+ * Function to parse the response from getting a list of events
+ * @param remoteEventResponse response gotten from the API
+ * @return Triple formed with the list of events, number of results and link to the next page of results (or null if it doesn't exist).
+ * The list of events doesn't include stages, since they must be parsed separately
+ */
+fun getEventList(remoteEventResponse: RemoteEventResponse): Triple<List<Event>, Long, String?>{
+    val eventList = remoteEventResponse.data.map(::getEvent)
+    return Triple(eventList, remoteEventResponse.total, remoteEventResponse.links.next?.href)
+}
--- shared/src/commonMain/kotlin/com/diegopalvarez/oreplay/domain/model/Event.kt
+++ shared/src/commonMain/kotlin/com/diegopalvarez/oreplay/domain/model/Event.kt
@@ -1,26 +1,26 @@
 package com.diegopalvarez.oreplay.domain.model
 
 import kotlinx.datetime.LocalDate
-import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
+import kotlin.time.Instant
 
 data class Event(
     val id: String,
     val isHidden: Boolean,
-    val picture: String,
-    val website: String,
+    val picture: String?,
+    val website: String?,
     val scope: String,
-    val location: String,
+    val location: String?,
     val countryCode: String,
     val initialDate: LocalDate,
     val finalDate: LocalDate,
     val timezone: TimeZone,
-    val federation: String,
-    val created: LocalDateTime,
-    val modified: LocalDateTime,
+    val federation: String?,
+    val created: Instant,       // A specific moment from the past should be represented as an Instant
+    val modified: Instant,
     val organizer: Organizer,
     val selfLink: String,
 
     // Stages
-    val stages: List<Stage>
+    val stages: List<Stage>? = null
 )
