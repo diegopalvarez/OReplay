c946b22cdf8dce488b81e059c333a7a4ee9384ac
Diego <diegopalvarez@gmail.com>
2026-08-01 19:54:46 +0200

    feat: set up the navigation from the Root Component and pass it as a parameter with platform-specific code.
    It's also necessary to add a new dependency to the AndroidApp

--- androidApp/src/main/kotlin/com/diegopalvarez/oreplay/MainActivity.kt
+++ androidApp/src/main/kotlin/com/diegopalvarez/oreplay/MainActivity.kt
@@ -4,23 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.diegopalvarez.oreplay.app.App
+import com.diegopalvarez.oreplay.app.navigation.RootComponent
+import com.arkivanov.decompose.defaultComponentContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
+        val root = RootComponent(
+            componentContext = defaultComponentContext()
+        )
+
         setContent {
-            App()
+            App(root = root)
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
--- shared/src/iosMain/kotlin/com/diegopalvarez/oreplay/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/diegopalvarez/oreplay/MainViewController.kt
@@ -1,11 +1,22 @@
 package com.diegopalvarez.oreplay
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import com.diegopalvarez.oreplay.app.App
 import com.diegopalvarez.oreplay.app.di.initKoin
+import com.diegopalvarez.oreplay.app.navigation.RootComponent
 
 fun MainViewController() = ComposeUIViewController(
     configure = {
         initKoin()
     }
-) { App() }
+) {
+    val root = remember {
+        RootComponent(DefaultComponentContext(LifecycleRegistry()))
+    }
+    App(
+        root = root
+    )
+}
