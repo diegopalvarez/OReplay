ef22a6111d8d1a6d225e2be437ef3943ea01ef59
Diego <diegopalvarez@gmail.com>
2026-08-30 17:42:43 +0200

    feat: delete unused template components

--- shared/src/commonMain/kotlin/com/diegopalvarez/oreplay/app/navigation/ScreenAComponent.kt
+++
@@ -1,9 +0,0 @@
-package com.diegopalvarez.oreplay.app.navigation
-
-import com.arkivanov.decompose.ComponentContext
-
-class ScreenAComponent(
-    componentContext: ComponentContext
-): ComponentContext by componentContext {
-
-}
