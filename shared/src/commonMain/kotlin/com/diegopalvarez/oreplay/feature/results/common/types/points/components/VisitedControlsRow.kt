b4ac2b391da982c9b5ffd477ccbbffcb96f657fd
Diego <diegopalvarez@gmail.com>
2026-09-10 11:51:47 +0200

    fix: UI padding in score points row

--- shared/src/commonMain/kotlin/com/diegopalvarez/oreplay/feature/results/common/types/points/components/VisitedControlsRow.kt
+++ shared/src/commonMain/kotlin/com/diegopalvarez/oreplay/feature/results/common/types/points/components/VisitedControlsRow.kt
@@ -1,7 +1,9 @@
 package com.diegopalvarez.oreplay.feature.results.common.types.points.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import com.diegopalvarez.oreplay.domain.model.StageResult
 import com.diegopalvarez.oreplay.domain.repository.util.ScoreResultStats
 import kotlin.collections.component1
@@ -16,7 +18,9 @@ fun VisitedControlsRow(
     val runnerControls = result.splits.map { it.control.station }
 
     // Show the row of controls
-    Row{
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+    ){
         // TODO - Find a better way to exclude the Finish control
         for ((control, number) in visitedStats.visited.entries.filterNot { it.key == "Finish" } .sortedBy { it.key.toLong() }) {
             val isVisited = runnerControls.contains(control)
