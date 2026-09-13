0e2820e148bbee4cdaf3629277c19b336724f736
Diego <diegopalvarez@gmail.com>
2026-09-14 01:05:27 +0200

    fix: native iOS SQLDelight driver needs a synchronous Schema

--- shared/src/iosMain/kotlin/com/diegopalvarez/oreplay/data/local/DatabaseFactory.kt
+++ shared/src/iosMain/kotlin/com/diegopalvarez/oreplay/data/local/DatabaseFactory.kt
@@ -1,12 +1,13 @@
 package com.diegopalvarez.oreplay.data.local
 
+import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 
 class IOSDatabaseDriverFactory() : DatabaseDriverFactory {
     override suspend fun createDriver(): SqlDriver {
         return NativeSqliteDriver(
-            LocalDatabase.Schema,
+            LocalDatabase.Schema.synchronous(),
             "local.db"
         )
     }
