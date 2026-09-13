34a8648b8a9b0e2ec84a8f205c4dd723706c0681
Diego <diegopalvarez@gmail.com>
2026-09-14 00:04:32 +0200

    fix: change web database factory function

--- shared/src/webMain/kotlin/com/diegopalvarez/oreplay/data/local/DatabaseFactory.kt
+++ shared/src/webMain/kotlin/com/diegopalvarez/oreplay/data/local/DatabaseFactory.kt
@@ -2,21 +2,17 @@ package com.diegopalvarez.oreplay.data.local
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.worker.WebWorkerDriver
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import org.w3c.dom.Worker
+import app.cash.sqldelight.driver.worker.createDefaultWebWorkerDriver
+import app.cash.sqldelight.driver.worker.expected.Worker
 import kotlin.js.ExperimentalWasmJsInterop
 import kotlin.js.js
 
 class WebDatabaseDriverFactory() : DatabaseDriverFactory {
-    val scope = CoroutineScope(Dispatchers.Default)
-
     override suspend fun createDriver(): SqlDriver {
-        return WebWorkerDriver(worker())        // The Web implementation of a DataBase doesn't have a file since it doesn't survive reloads
+        // The Web implementation of a DataBase doesn't have a file since it doesn't survive reloads
+        return createDefaultWebWorkerDriver()
+            .also {
+                LocalDatabase.Schema.create(it).await()
+            }
     }
-
-    // The JS block must be declared in a separate function to work
-    @OptIn(ExperimentalWasmJsInterop::class)
-    fun worker(): app.cash.sqldelight.driver.worker.expected.Worker =
-        js("""new URL("@cashapp/sqldelight-sqljs-worker/sqljs.worker.js", import.meta.url)""")
 }
