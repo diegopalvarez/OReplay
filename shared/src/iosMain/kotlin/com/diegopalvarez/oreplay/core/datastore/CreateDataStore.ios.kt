07ce8532ecb51679386863f5a1763c9b32477849
Diego <diegopalvarez@gmail.com>
2026-09-13 21:49:36 +0200

    feat: update createDataStore implementation and support web targets

--- shared/src/iosMain/kotlin/com/diegopalvarez/oreplay/core/datastore/CreateDataStore.ios.kt
+++ shared/src/iosMain/kotlin/com/diegopalvarez/oreplay/core/datastore/CreateDataStore.ios.kt
@@ -1,8 +1,12 @@
 package com.diegopalvarez.oreplay.core.datastore
 
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.okio.OkioStorage
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.PreferencesSerializer
 import kotlinx.cinterop.ExperimentalForeignApi
+import okio.FileSystem
+import okio.Path.Companion.toPath
 import platform.Foundation.NSDocumentDirectory
 import platform.Foundation.NSFileManager
 import platform.Foundation.NSUserDomainMask
@@ -10,15 +14,19 @@ import platform.Foundation.NSUserDomainMask
 @OptIn(ExperimentalForeignApi::class)
 fun createDataStore(): DataStore<Preferences> {
     return createDataStore(
-        producePath = {
-            val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
-                directory = NSDocumentDirectory,
-                inDomain = NSUserDomainMask,
-                appropriateForURL = null,
-                create = false,
-                error = null
-            )
-            requireNotNull(documentDirectory).path + "/prefs.preferences_pb"
-        }
+        storage = OkioStorage(
+            fileSystem = FileSystem.SYSTEM,
+            serializer = PreferencesSerializer,
+            producePath = {
+                val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
+                    directory = NSDocumentDirectory,
+                    inDomain = NSUserDomainMask,
+                    appropriateForURL = null,
+                    create = false,
+                    error = null
+                )
+                (requireNotNull(documentDirectory).path + "/$dataStoreFileName").toPath()
+            }
+        )
     )
 }
