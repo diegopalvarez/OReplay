791c8937ade23d5d9303687672430dc3429b114f
Diego <diegopalvarez@gmail.com>
2026-09-13 20:09:56 +0200

    feat: first version of the root component creation for the common Web Main entry point
    TODO: https://github.com/arkivanov/Decompose/discussions/644

--- webApp/src/webMain/kotlin/com/diegopalvarez/oreplay/main.kt
+++ webApp/src/webMain/kotlin/com/diegopalvarez/oreplay/main.kt
@@ -2,11 +2,29 @@ package com.diegopalvarez.oreplay
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.arkivanov.essenty.lifecycle.resume
 import com.diegopalvarez.oreplay.app.App
+import com.diegopalvarez.oreplay.app.navigation.RootComponent
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    // Create Root Component
+    val lifecycle = LifecycleRegistry()
+
+    val root = RootComponent(
+        // Pass the LifecycleRegistry to the context
+        componentContext = DefaultComponentContext(lifecycle = lifecycle),
+    )
+
+    // Resume the lifecycle
+    lifecycle.resume()
+
+    // Render the UI
     ComposeViewport {
-        App()
+        App(
+            root = root,
+        )
     }
 }
